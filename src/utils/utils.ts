cb3563dafec050e90febbbd36867a7061c50c4d0
alexeiTruhin <alexei.truhin@gmail.com>
2019-12-13 23:57:49 +0200

    feat(utils): add throttle function

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,8 +1,12 @@
 
-export function format(first: string, middle: string, last: string): string {
-  return (
-    (first || '') +
-    (middle ? ` ${middle}` : '') +
-    (last ? ` ${last}` : '')
-  );
+export function throttle(func: Function, wait: number) {
+  let stop = false; 
+  
+  return function(...args) {
+    if (!stop) {
+      func.apply(this, args);
+      stop = true;
+      setTimeout(function(){stop = false}, wait);
+    }
+  }
 }
